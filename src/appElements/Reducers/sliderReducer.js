89ee84290f3910ce8c52317cb8e154f133da65b8
Azer N. Karimzada <KarimzadaAN@kapitalho.az>
2020-05-15 02:25:08 +0400

    15.05.2020

--- src/appElements/Reducers/sliderReducer.js
+++ src/appElements/Reducers/sliderReducer.js
@@ -10,11 +10,11 @@ let sliderReducer=(state=initialState,action,id)=>{
 
     switch(action.type){
         case 'slide':
-           if( action.action==='next'){
+           if( action.action==='next'&&state.index<=8972){
                return {...state,
                 index:state.index+1}
            }
-           else if(action.action==='previous'){
+           else if(action.action==='previous'&&state.index>0){
                return {...state,
                 index:state.index-1}
            }
