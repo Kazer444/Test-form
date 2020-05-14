4a2f942a9bc2a0c11671936c72e2434a7a502068
Azer N. Karimzada <KarimzadaAN@kapitalho.az>
2020-05-15 01:34:41 +0400

    Updates

---
+++ precache-manifest.944eecafca6593ee0fe3356cd9bb604b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2701b8374b08ad9871dc3e5ba0e3e4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1428f5bb127e7d9a26ba",
+    "url": "/static/css/main.2e18268f.chunk.css"
+  },
+  {
+    "revision": "912470abc13ec55f388d",
+    "url": "/static/js/2.8bfaf42a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.8bfaf42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1428f5bb127e7d9a26ba",
+    "url": "/static/js/main.2a06a43b.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
