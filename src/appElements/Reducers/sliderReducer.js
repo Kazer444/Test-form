f461590094c16821f646a732f596eadae03452bd
Azer N. Karimzada <KarimzadaAN@kapitalho.az>
2020-05-17 18:16:57 +0400

    a

--- src/appElements/Reducers/sliderReducer.js
+++ src/appElements/Reducers/sliderReducer.js
@@ -7,16 +7,16 @@ let initialState={
 
 
 let sliderReducer=(state=initialState,action,id)=>{
-
+debugger
     switch(action.type){
         case 'slide':
            if( action.action==='next'&&state.index<=8972){
                return {...state,
-                index:state.index+1}
+                index:Number(state.index)+1}
            }
            else if(action.action==='previous'&&state.index>0){
                return {...state,
-                index:state.index-1}
+                index:Number(state.index)-1}
            }
        
         case 'numSelector':
